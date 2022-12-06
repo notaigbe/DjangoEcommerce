bfb0654a17baef367e534a1cb4e90e578cf4437a
notaigbe <notaigbe@gmail.com>
2022-12-06 10:11:50 +0100

    MVP for Farmfresh - set up custom domain

--- DjangoEcommerce/settings.py
+++ DjangoEcommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'urchin-app-pwz4d.ondigitalocean.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'urchin-app-pwz4d.ondigitalocean.app', 'farmfresh.tacommodity.com']
 
 
 # Application definition
