247b002eb19ca579df3c9030d74457c323668a13
notaigbe <notaigbe@gmail.com>
2022-12-07 21:46:57 +0100

    MVP for Farmfresh - added password reset templates

--- DjangoEcommerce/settings.py
+++ DjangoEcommerce/settings.py
@@ -126,6 +126,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST_USER = 'postmaster@tacommodity.com'
+EMAIL_HOST_PORT = 587
+EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 
