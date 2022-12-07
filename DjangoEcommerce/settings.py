1cfbca7d1123c0aa83a14d81f38c3b5b60131997
notaigbe <notaigbe@gmail.com>
2022-12-07 18:17:55 +0100

    MVP for Farmfresh - set up postgresql database

--- DjangoEcommerce/settings.py
+++ DjangoEcommerce/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+
+import dj_database_url
+from django.conf.global_settings import DATABASES
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -87,12 +90,8 @@ WSGI_APPLICATION = 'DjangoEcommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] = dj_database_url.config(
+    default=os.environ.get('DATABASE_URL'))
 
 
 # Password validation
